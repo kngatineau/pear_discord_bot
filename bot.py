b63ca8e997a235ec29898a75d966e0d3051e6df0
Kaitlyn Gatineau <gati0010@algonquinlive.com>
2022-08-10 15:27:51 -0600

    Added user input for reaction and logic, bot auto reacting and more

--- bot.py
+++ bot.py
@@ -1,76 +1,120 @@
 # bot.py
+
 import os
 import random
 
 import discord
-# from discord.ext.commands import bot
 
 from dotenv import load_dotenv
 from discord.ext import commands
 
+#  Loads the token data from the .env file and sets the 'TOKEN' variable
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+#  Intents required to retrieve member data as per the Discord API update
 intents = discord.Intents.default()
 intents.members = True
 
+#  sets the 'bot' and 'client' variables and prefix to trigger the bot to '!'
 bot = commands.Bot(command_prefix="!", intents=intents)
 client = discord.Client()
 
 
+#  event that launches automatically when the bot runs/connects
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 
+#  Command triggered via '!create'
 @bot.command()
-async def create(ctx, title, description):
+@commands.has_permissions(administrator=True)
+async def create(ctx, title, description, emoji):
+    #  generates the embed to be sent as a response
     embed = discord.Embed(title=title,
                           description=description,
                           color=0xFF5733)
+
+    #  initializes the channel to send the message in
     channel = bot.get_channel(channel_id)
 
-    await ctx.send(embed=embed)
+    #  sends the embed to the channel the command was triggered in
+    msg = await ctx.send(embed=embed)
+    await msg.add_reaction(emoji)
+
+    print(str(emoji))
+
+    #  writes the ID of the message created to a .txt file for later use
     message = await channel.fetch_message(channel.last_message_id)
-    with open('root_message.txt', 'w') as f:
+    with open('root_message.txt', 'w', encoding="utf-8") as f:
         f.write(str(message.id))
+        f.write("\n" + str(emoji))
 
 
-channel_id = 976502346878177293  # Replace with channel id
+#  currently hard coded to an ID in a test server, will move these to the .env or .txt file next iteration
+channel_id = 976502346878177293
 
 
-#  message_id = 984196833792364594  # Note these are ints, not strings
-
-
+#  Command triggered via '!pair"
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def pair(ctx):
-    channel = bot.get_channel(channel_id)
+    #  opens the .txt file and reads the message ID of the reaction host post
     with open('root_message.txt') as r:
-        message_id = int(r.read())
+        message_id = int(r.readline())
+        reaction_emoji = str(r.readline())
+    print("Reaction emoji:" + reaction_emoji)
+    #  sets the respective variables
+    channel = bot.get_channel(channel_id)
     message = await channel.fetch_message(message_id)
+
+    #  sets an empty array to be filled with users
     users = set()
-    guild = bot.get_guild(976502346878177290)
 
+    #  await message.add_reaction(emoji)
+
+    #  loops over the reactions on the specified post
     for reaction in message.reactions:
-        async for user in reaction.users():
-            if guild.get_member(user.id) is None:
-                await reaction.remove(user)
-            else:
-                users.add(user)
 
+        #  loops over the users for each reaction
+        async for user in reaction.users():
+            guild = bot.get_guild(976502346878177290)
+
+            if guild.get_member(user.id) is None:
+                #  deletes a user if they no longer are a part of the server
+                await reaction.remove(user)
+
+            else:
+                #  otherwise, adds them to the list
+                if user.id != 949802757483810917:
+                    users.add(user)
+
+    #  copies the value of the list 'users' too 'userslist' so it can be shuffled, then shuffles the list
     userslist = list(users)
     random.shuffle(userslist)
-    breakline = "\n"
-    emoji = '\U0001F538'
 
-    for user in userslist:
+    #  loops over the shuffled list of users
+    if len(userslist) > 2:
+        #  variables set as these values cannot be added directly to f string statements
+        breakline = "\n"
+        emoji = '\U0001F538'
+
+        for _ in userslist:
+            #  adds the list values to the 'pair' embed
+            embed = discord.Embed(title="Coffee Chat Pairings",
+                                  description=f"Pairings for this week are:\n"
+                                              + f" {' '.join(user.mention + breakline if i % 2 != 0 else user.mention + emoji for i, user in enumerate(userslist))} ",
+                                  color=0xFF5733)
+            await ctx.send(embed=embed)
+    else:
         embed = discord.Embed(title="Coffee Chat Pairings",
-                              description=f"Pairings for this week are:\n"
-                                          + f" {' '.join(user.mention + breakline if i % 2 != 0 else user.mention + emoji for i, user in enumerate(userslist))} ",
+                              description="No pairings available.",
                               color=0xFF5733)
+        await ctx.send(embed=embed)
 
-
-    await ctx.send(embed=embed)
+    #  sends the embed
+    #  await ctx.send(embed=embed)
 
 
 bot.run(TOKEN)
